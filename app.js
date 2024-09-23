2f870d9787583debf788d63cd72af8f733a9af12
Исхак Мурадилдеев <iskhak@MacBook-Air-Ishak.local>
2024-09-23 23:50:48 +0600

    Initial Commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const ApiKey = require("./middleware/apiKeyMiddleware")
+const app = require("express")()
+const bodyParser = require("body-parser")
+const mangaRouter = require("./routes/mangaRouter")
+const mangaListRouter = require("./routes/mangaListRouter")
+const mangaSearch = require("./routes/mangaSearch")
+let cors = require('cors')
+
+
+app.use(bodyParser.json())
+app.use(cors())
+require('dotenv').config()
+
+
+app.use(ApiKey)
+app.use("/api/manga", mangaRouter)
+app.use("/api/mangaList", mangaListRouter)
+app.use("/api/search", mangaSearch)
+
+app.listen(process.env.PORT, ()=>{
+    console.log(`Server Start On Port ${process.env.PORT} 🎉✨ `)
+})
